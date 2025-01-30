226d7b742d0ffae5c3557c1526829bc06b629185
jitender <muwal.jitender@gmail.com>
2025-01-30 16:11:24 +0100

    feat: Add style to the navbar.
    - The responsive changes are pending

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,31 +1,40 @@
-import "./Navbar.css"; // Assuming you have some CSS for styling
+import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { IoIosMoon, IoMdSunny } from "react-icons/io";
+import { Link, NavLink } from "react-router-dom";
+
+import { AiOutlineClose } from "react-icons/ai";
+import { HiOutlineBars3 } from "react-icons/hi2";
 import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">Election Voting App</Link>
+    <nav className="nav">
+      <div className="container nav__container">
+        <Link to="/" className="nav__logo">
+          Election Voting App
+        </Link>
+
+        <div>
+          <menu>
+            <NavLink to="/elections" className="nav-link">
+              Elections
+            </NavLink>
+            <NavLink to="/results" className="nav-link">
+              Results
+            </NavLink>
+            <NavLink to="/logout" className="nav-link">
+              Logout
+            </NavLink>
+          </menu>
+          <button className="theme__toggle-btn">
+            <IoIosMoon />
+          </button>
+          <button className="nav__toggle-btn">
+            <HiOutlineBars3 />
+          </button>
+        </div>
       </div>
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link to="/home" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-link">
-            Contact
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 };
