c05961cbe2839c7b157a9d771f6a9cb076ba7d36
jitender <muwal.jitender@gmail.com>
2025-02-12 23:06:38 +0100

    feat: Complete the Congrats page after successful vote casting

--- client/src/pages/Congrats.tsx
+++ client/src/pages/Congrats.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import "./Congrats.css";
+
+import { Link } from "react-router-dom";
 
 const Congrats = () => {
-  return <div>Congrats</div>;
+  return (
+    <section className="congrats">
+      <div className="container congrats__container">
+        <div className="congrats__content">
+          <h2>Thanks for your vote!</h2>
+          <p>
+            Your vote has been successfully casted. Please follow the below link
+            to see the result.
+          </p>
+          <Link to="/results" className="btn sm primary">
+            See Results
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Congrats;
