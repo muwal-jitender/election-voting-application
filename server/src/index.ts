1eb752fceee2cf58be5634a2aec014d69f7a943e
jitender <muwal.jitender@gmail.com>
2025-02-17 14:35:38 +0100

    feat: Setup the server project. - Add the src folder and index.ts (entering point) - Add .env and gitignore file - Add base NPM packages

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+
+import {connect} from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors({credentials: true, origin:"http://localhost:3000"}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Test route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server is running!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
