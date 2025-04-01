c3bf5d7f8cef841a8a79c3ce735620bc4fc1634a
jitender <muwal.jitender@gmail.com>
2025-04-01 08:21:20 +0200

    fix: Remove console.log

--- client/src/components/layout/ProtectedRoute.tsx
+++ client/src/components/layout/ProtectedRoute.tsx
@@ -4,7 +4,6 @@ import { useUser } from "context/UserContext";
 
 const AdminRoute = () => {
   const { isAdmin, loading, user } = useUser();
-  console.log("AdminRoute", { isAdmin, loading, user });
 
   if (loading) return <></>;
   if (!user) return <Navigate to="/" replace />; // ✅ If not logged in then redirect to login page, `replace` is preventing Backwards Navigation
--- client/src/utils/theme.utils.ts
+++ client/src/utils/theme.utils.ts
@@ -5,8 +5,6 @@ export const setTheme = (setDarkTheme: (name: THEME) => void) => {
   const currentTheme = localStorage.getItem(VOTING_APP_THEME) as THEME;
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   localStorage.setItem(VOTING_APP_THEME, newTheme);
-  console.log("Theme toggled to:", newTheme);
-
   setDarkTheme(newTheme);
 };
 
