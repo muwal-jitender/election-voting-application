ab4fec90e9801c9bbd489d8a061ac78afa4fa245
jitender <muwal.jitender@gmail.com>
2025-04-15 12:50:10 +0200

    feat: make LogoIcon responsive on mobile using CSS media query
    - Added `.logo-icon` class to SVG in LogoIcon component
    - Applied width: 200px by default, reduced to 155px on screens < 600px
    - Ensures better layout and visual consistency on mobile devices

--- client/src/components/ui/LogoIcon.tsx
+++ client/src/components/ui/LogoIcon.tsx
@@ -1,8 +1,9 @@
+import "./LogoIcon.css"; // Import the CSS file for styling
+
 const LogoIcon = () => {
   return (
     <svg
-      width="200"
-      height="48"
+      className="logo-icon"
       viewBox="0 0 200 50"
       xmlns="http://www.w3.org/2000/svg"
       role="img"
